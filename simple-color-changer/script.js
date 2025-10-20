7babc4ef4d8318e52cc91bc32c0675c8bb075aff
th3-saint <mrmedrobaii@gmail.com>
2025-10-20 11:26:46 +0000

    simple color changer

---
+++ simple-color-changer/script.js
@@ -0,0 +1,18 @@
+
+const container = document.getElementById("container")
+
+function changeGreen() {
+    container.style.backgroundColor = "#06e906"
+}
+
+function changeRed() {
+    container.style.backgroundColor = "#e90606"
+}
+
+function changeBlue() {
+    container.style.backgroundColor = "#0a06e9"
+}
+
+function Reset() {
+    container.style.backgroundColor = "#ffff"
+}
